2f70a55ce3d1461086125b8ba74a06a5e26c20b0
GlebGus <gusinec.lion.com@tut.by>
2022-07-22 05:10:35 +0300

    added roles and permissions

--- src/main/java/com/example/springsecurity/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/springsecurity/security/ApplicationSecurityConfig.java
@@ -1,25 +1,70 @@
 package com.example.springsecurity.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
-import org.springframework.security.web.SecurityFilterChain;
-
-import static org.springframework.security.config.Customizer.withDefaults;
+import static com.example.springsecurity.student.ApplicationUserRole.ADMIN;
+import static com.example.springsecurity.student.ApplicationUserRole.STUDENT;
 
 @Configuration
 @EnableWebSecurity
-public class ApplicationSecurityConfig  {
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((authz) -> authz
-                        .antMatchers("/","index","/css/*","/js/*")
-                        .permitAll()
-                        .anyRequest().authenticated()
-                )
-                .httpBasic(withDefaults());
-        return http.build();
+
+public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+    private final PasswordEncoder passwordEncoder;
+
+@Autowired
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+      http
+              .authorizeRequests()
+              .antMatchers("/", "index", "/css/*", "/js/*")
+              .permitAll()
+              .anyRequest()
+              .authenticated()
+              .and()
+              .httpBasic();
+    }
+
+//    @Bean
+//@Autowired
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        http.authorizeHttpRequests((authz) -> authz
+//                        .antMatchers("/", "index", "/css/*", "/js/*")
+//                        .permitAll()
+//                        .anyRequest().authenticated()
+//                )
+//                .httpBasic(withDefaults());
+//        return http.build();
+//    }
+
+    @Override
+  @Bean
+    protected UserDetailsService userDetailsService() {
+        UserDetails annaSmithUser = User.builder()
+                .username("annasmith")
+                .password(passwordEncoder.encode("password"))
+                .roles(STUDENT.name())
+                .build();
+      UserDetails linda =  User.builder()
+                .username("Linda")
+                .password(passwordEncoder.encode("password123"))
+                .roles(ADMIN.name())
+                .build();
+
+        return new InMemoryUserDetailsManager(annaSmithUser, linda);
     }
 }
